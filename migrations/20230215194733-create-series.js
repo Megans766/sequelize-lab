b1ebcfad51fbe32df3c18c918ba1b0e597b46c1c
Megan Smith <megans766@gmail.com>
2023-02-15 14:58:59 -0500

    create series model & update model

---
+++ migrations/20230215194733-create-series.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Series', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      series: {
+        type: Sequelize.BOOLEAN
+      },
+      bookInSeries: {
+        type: Sequelize.INTEGER
+      },
+      authorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Authors',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Series');
+  }
+};
---
+++ models/series.js
@@ -0,0 +1,28 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Series extends Model {
+
+    static associate(models) {
+  
+    }
+  }
+  Series.init({
+    series: DataTypes.BOOLEAN,
+    bookInSeries: DataTypes.INTEGER,
+    authorId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Authors',
+        key: 'id'
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Series',
+  });
+  return Series;
+};
