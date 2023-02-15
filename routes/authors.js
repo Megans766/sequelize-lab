d5b6bd80a1faab9eedc108bc35a1b065f0fee67e
Megan Smith <megans766@gmail.com>
2023-02-15 14:23:37 -0500

    create route and controller function to update an author

--- routes/authors.js
+++ routes/authors.js
@@ -4,5 +4,6 @@ const authorsCtrl = require('../controllers/authors.js')
 //routes
 router.post('/', authorsCtrl.create)
 router.get('/', authorsCtrl.index)
+router.put('/:id', authorsCtrl.update)
 
 module.exports = router
